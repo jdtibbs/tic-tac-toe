06c792201a6d29dfa49d33214eba25a9df20115d
Jack Tibbs <jdtibbs@users.noreply.github.com>
2016-03-21 11:01:45 -0500

    make these fn private.

--- app/services/game.factory.js
+++ app/services/game.factory.js
@@ -6,28 +6,30 @@
 		game: function(context) {
 			var moves = [];
 
+			function isValidMove(cell) {
+				var move = moves.filter(function(element) {
+					if (element.row.id === cell.row.id && element.column.id === cell.column.id) {
+						return true;
+					}
+				});
+				return move.length === 0; // true, move not found.
+			}
+
+			function moved(cell) {
+				moves.push(cell);
+			}
+
 			return {
-				isValidMove: function(cell) {
-					var move = moves.filter(function(element) {
-						if (element.row.id === cell.row.id && element.column.id === cell.column.id) {
-							return true;
-						}
-					});
-					return move.length === 0; // true, move not found.
-				},
 				move: function(event) {
 					var cell = grid.onClick(event);
 					if (cell !== undefined) {
-						if (this.isValidMove(cell)) {
+						if (isValidMove(cell)) {
 							console.log('row: ' + cell.row.id + ' column: ' + cell.column.id);
-							this.moved(cell);
+							moved(cell);
 							grid.drawO(context(), cell);
 							grid.drawX(context(), cell);
 						}
 					}
-				},
-				moved: function(cell) {
-					moves.push(cell);
 				},
 				newGame: function() {
 					moves.length = 0;
