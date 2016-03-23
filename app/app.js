d0345c527599e810bb678cff01d08db7a77cccf6
Jack Tibbs <jdtibbs@users.noreply.github.com>
2016-03-23 15:23:01 -0500

    firebase authWithPassword

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@
 	var documentService = require('services/document.service');
 	var eventService = require('services/event.service');
 	var gameFactory = require('services/game.factory');
+	var firebaseFactory = require('services/firebase.factory');
 
 	module.exports = {
 		init: function() {
@@ -13,11 +14,32 @@
 			function onDOMLoaded() {
 				console.log('DOM has loaded.');
 				var context = canvasService.contextFactory('grid');
+				var firebase = firebaseFactory.create();
 				var game = gameFactory.game(context);
 				game.newGame();
 
-				eventService.addEventListener(documentService.getElementById('grid'), 'click', gridClick);
-				eventService.addEventListener(documentService.getElementById('newGame'), 'click', newGame);
+				eventService.addEventListener(documentService.getElementById('login-btn'), 'click', login);
+
+				function login(event) {
+					var email = documentService.getElementById('email');
+					firebase.authWithPassword(email.value, 'tictactoe', loginSuccess, loginError);
+				}
+
+				function loginSuccess() {
+					documentService.getElementById('login-error').textContent = "";
+					var name = documentService.getElementById('name');
+					documentService.getElementById('login-success').textContent = name.value + ", Welcome to Tic-Tac-Toe!";
+					window.setTimeout(function() {
+						documentService.getElementById('login-success').textContent = "";
+					}, 3000);
+					eventService.addEventListener(documentService.getElementById('grid'), 'click', gridClick);
+					eventService.addEventListener(documentService.getElementById('newGame'), 'click', newGame);
+				}
+
+				function loginError(error) {
+					documentService.getElementById('login-error').textContent = error;
+					documentService.getElementById('login-success').textContent = "";
+				}
 
 				function gridClick(event) {
 					game.move(event);
